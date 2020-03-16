ea73cf466e2440f69f8769a8a0f1b58c2f420782
Victor Onofiok <radoncreep@gmail.com>
2020-03-16 22:19:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Building a mailer application for sending mails to users/customers
+
+* [ ] Setup express.js app server
+    * [ ] Download all necessary dependencies.
+    * [ ] Setup middlewares
+
+* [ ] Create public module for html, css, Js and bootstrap files
+
+* [ ] create views module for ejs files
+    * [ ] Setup middleware that is used to locate files from the views module.
+
+* [ ] Create routing module 
+
+* [ ] Require auth-key from sendgrid to use sendgrid's API.
+
+* [ ] 
---
+++ index.js
@@ -0,0 +1,103 @@
+//core modules 
+const path = require('path');
+
+//third-party  modules 
+const express = require('express');
+const bodyParser = require('body-parser');
+const sendgrid = require('@sendgrid/mail');
+// var inputTokenizer = require("input-tokenizer");
+
+//middleware
+const app = express();
+
+//view engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+sendgrid.setApiKey('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');
+//routes
+
+
+app.get('/', (req, res, next) => {
+    res.send('index', { pageTitle: 'Exolve Emailer' });
+});
+
+app.post('/send', (req, res, next) => {
+
+    const sender = req.body.fromsender; 
+    const receiver = req.body.toemail;
+    const subject = req.body.subject;
+    const message = req.body.message;
+
+
+    //this for sending html documents
+    const output = `
+        // insert html code here
+    `;
+
+
+    const emailString = req.body.to; //the data is stored in the const
+
+    const emailArray = emailString.split(","); //the data is split here
+
+    if (emailArray.length > 1) {
+
+        //use loop
+        emailArray.forEach(function (item, index) {
+
+            sendgrid.send({
+                to: item.trim(),
+                from: sender,
+                subject: subject,
+                message: message,
+                html: output
+            })
+        });
+
+    } else {
+        // send
+        sendgrid.send({
+            to: receiver,
+            from: sender,
+            subject: subject,
+            message: message,
+            html: output
+
+        },
+            function (err, json) {
+                if (err) {
+                    return res.send('bs');
+                }
+                res.send('<h1>Email sent</h1>');
+                res.redirect('/');
+            }
+        )
+    }
+    // sendgrid.send({
+    //     personalizations: [{
+    //         to: [{
+    //             email: req.body.to
+    //         },        ]  
+    //     }],
+    //     from: 'exolveTech@gmail.com',
+    //     subject: 'mail from sendgrid',
+    //     html: output
+
+    // }, 
+    // function(err, json) {
+    //     if(err) {
+    //         return res.send('bs');
+    //     }
+    //     res.send('<h1>Email sent</h1>');
+    // }
+    // )
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(5000, () => console.log(`Server is running on ${PORT}`));
