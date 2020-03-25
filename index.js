98eab58fd70fb5bd4bd55090d29c60b74ff51d91
Victor Onofiok <48057500+radoncreep@users.noreply.github.com>
2020-03-25 23:51:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-sendgrid.setApiKey('################');
+sendgrid.setApiKey(process.env.API_KEY);
 //routes
 
 
